42d91a74c25c377ce5877021458934658760535d
mccleskeyc <mccleskeyc@gmail.com>
2020-12-30 15:33:20 -0500

    fixed typo error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
-    set :session_secret ENV['SESSION_SECRET']
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get "/" do
